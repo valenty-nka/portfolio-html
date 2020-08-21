04fa61313bdcb55874daea87747b29e56fe0731c
valenty-nka <69852274+valenty-nka@users.noreply.github.com>
2020-08-21 14:16:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+var mySwiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".projects-pagination",
+    bulletClass: "projects-bullet",
+    bulletActiveClass: "projects-bullet-active",
+    clickable: true,
+  },
+});
